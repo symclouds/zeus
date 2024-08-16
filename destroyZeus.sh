8e5191adf62e4e972846ff3630357898980d8ad9
Ilir Iljazi <iljazi@gmail.com>
2024-08-16 15:16:55 +0200

    Testing the destroyZeus.sh script

--- destroyZeus.sh
+++ destroyZeus.sh
@@ -28,7 +28,7 @@ echo "* >>> Starting zeus destruction tool using aws cdk *"
 echo "****************************************************"
 echo ""
 echo ">>> Destroying stack/s ... Please be patient "
-T=$(cd cdk && echo y | cdk destroy --all --require-approval never --profile ${profile})
+T=$(echo y | cdk destroy --all --require-approval never --profile ${profile})
 if [ $? -ne 0 ]
 then    
     echo ">>> Error: Problem with destroying Zeus from AWS"
@@ -37,3 +37,25 @@ then
 else
     echo ">>> ... Successfully destroyed Zeus resources from AWS"
 fi
+
+echo ""
+echo "*** Note: If you want to destroy the CloudFormation Stack created by the cdk construct"
+echo "          and clean up your enviroment from any artifacts left over from that process,"
+echo "          please run the following command from aws cli: "
+echo ""
+echo "          #aws cloudformation delete-stack --stack-name CDKToolkit --profile <profile>"
+echo "          (where profile contains the necessary credentials required to delete the stack)"
+echo ""
+echo "          The command above does not delete the s3 bucket created by the CDKToolkit Stack,"
+echo "          therefore any residual assets and resources that are obsolete may continue to"
+echo "          rack up storage costs unnecessarily. If you no longer have a need for these assets,"
+echo "          you must explicitly delete them from the console or the cli:"
+echo ""
+echo "          1. Empty Bucket of objects:"
+echo "              aws s3 rm s3://cdk-<string>-assets-<account id>-<region> --recursive --profile <profile>"
+echo "          2. Delete Bucket:"
+echo "              aws s3 rm s3://cdk-<string>-assets-<account id>-<region> --profile <profile>"
+echo "          (where profile contains the necessary credentials required to delete the stack)"
+echo ""
+
+
