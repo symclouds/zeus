44f03eeff289bb76894cf91bc86f779d8d8483dc
Ilir Iljazi <iljazi@Ilirs-MacBook-Pro-M2.local>
2024-09-03 18:58:40 +0200

    testing zeusDeploy zeusDestroy node scripts

--- bin/temp/zeus.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-export declare class Zeus extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps);
-}
